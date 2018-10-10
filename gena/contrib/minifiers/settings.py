f8577d8274fa9e0bdcef53e747ebafbd4d9a3a4e
Dmitry Pakhomov <dp@dspro.org>
2018-10-10 10:44:23 +0300

    Minifiers: Remove DEFAULT_HTML_MINIFIER_OPTIONS

--- gena/contrib/minifiers/settings.py
+++ gena/contrib/minifiers/settings.py
@@ -4,10 +4,8 @@ from gena.settings import settings
 
 
 # See possible options https://htmlmin.readthedocs.io/en/latest/reference.html#main-functions
-DEFAULT_HTML_MINIFIER_OPTIONS = {
+HTML_MINIFIER_OPTIONS = settings.get('HTML_MINIFIER_OPTIONS', {
     'reduce_boolean_attributes': True,
     'remove_comments': True,
     'remove_empty_space': True,
-}
-
-HTML_MINIFIER_OPTIONS = settings.get('HTML_MINIFIER_OPTIONS', DEFAULT_HTML_MINIFIER_OPTIONS)
+})
