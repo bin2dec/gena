cb5fd602c452be4df05e5d90d130e63b6ede60ee
Dmitry Pakhomov <dp@dspro.org>
2018-10-27 18:12:50 +0300

    CLI: Add a run command

--- gena/__main__.py
+++ gena/__main__.py
@@ -4,6 +4,7 @@ import sys
 import sysconfig
 
 from argparse import ArgumentParser
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from time import time
 
 from gena import __version__
@@ -12,6 +13,25 @@ from gena.settings import settings
 
 
 logger = logging.getLogger(__name__)
+
+
+class HTTPRequestHandler(SimpleHTTPRequestHandler):
+    error_logging = False
+    request_logging = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=settings.DST_DIR, **kwargs)
+
+    def log_error(self, format, *args):
+        if self.error_logging:
+            super().log_error(format, *args)
+
+    def log_message(self, format, *args):
+        logger.debug('%s', format % args)
+
+    def log_request(self, code='-', size='-'):
+        if self.request_logging:
+            super().log_request(code, size)
 
 
 def _build(args):
@@ -36,6 +56,37 @@ def _build(args):
     else:
         if not args.log_level == logging.CRITICAL:
             print(f'Finished in {time() - start_time:.2f} sec. with {files} file(s) processed')
+
+
+def _run(args):
+    if args.src is not None:
+        settings.SRC_DIR = args.src
+
+    if args.dst is not None:
+        settings.DST_DIR = args.dst
+
+    try:
+        runner = utils.import_attr(settings.RUNNER)
+        runner = runner()
+        runner.run()
+
+        if not args.log_level == logging.CRITICAL:
+            print(f'Starting an HTTP server at http://{args.address}:{args.port}/\n'
+                  f'Press Ctrl-C to stop the server')
+
+        with HTTPServer((args.address, args.port), HTTPRequestHandler) as httpd:
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                if not args.log_level == logging.CRITICAL:
+                    print('Stopping the HTTP server')
+    except Exception as exception:
+        logger.critical(exception)
+
+        if settings.DEBUG:
+            raise
+        else:
+            sys.exit(1)
 
 
 def main():
@@ -114,6 +165,40 @@ def main():
     )
 
     build.set_defaults(func=_build)
+
+    run = subparsers.add_parser(
+        'run',
+        help='run a simple HTTP server to serve files from the destination directory',
+    )
+
+    run.add_argument(
+        'src',
+        help='the directory that contains source files (default: src)',
+        metavar='source',
+        nargs='?',
+    )
+
+    run.add_argument(
+        'dst',
+        help='the directory for processed files (default: dist)',
+        metavar='destination',
+        nargs='?',
+    )
+
+    run.add_argument(
+        '-a', '--address',
+        default='127.0.0.1',
+        help='the address on which the server is listening (default: 127.0.0.1)',
+    )
+
+    run.add_argument(
+        '-p', '--port',
+        default=8000,
+        help='the listening port (default: 8000)',
+        type=int,
+    )
+
+    run.set_defaults(func=_run)
 
     args = parser.parse_args()
 
