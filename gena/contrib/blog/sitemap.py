b8534a7eac8a522ac6474aea9f7d06e92fc3f181
Dmitry Pakhomov <dp@dspro.org>
2018-12-21 15:47:36 +0300

    Blog: Add a loc property to SitemapEntry

--- gena/contrib/blog/sitemap.py
+++ gena/contrib/blog/sitemap.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+import urllib.parse
+
+from xml.sax import saxutils
 
 from gena.context import context
 from gena.settings import settings
@@ -12,12 +14,31 @@ __all__ = (
 )
 
 
+LOC_SIZE = 2_048
+
+
 def add_sitemap_entry_to_context(loc: str, **kwargs) -> None:
     if settings.BLOG_SITEMAP:
         sitemap_entry = SitemapEntry(loc, **kwargs)
         context.add_to_list(settings.BLOG_CONTEXT_SITEMAP, sitemap_entry)
 
 
-@dataclass()
 class SitemapEntry:
-    loc: str
+    def __init__(self, loc: str):
+        self.loc = loc
+
+    @property
+    def loc(self) -> str:
+        return self._loc
+
+    @loc.setter
+    def loc(self, value: str) -> None:
+        url = urllib.parse.urlsplit(value)
+        path = urllib.parse.quote(url.path, safe='/&\'"><=')
+        path = saxutils.escape(path, {'\'': '&apos;', '"': '&quot;'})
+        new_url = urllib.parse.urlunsplit((url.scheme, url.netloc, path, url.query, url.fragment))
+
+        if len(new_url) > LOC_SIZE:
+            raise ValueError(f'a sitemap URL must be less than {LOC_SIZE} characters')
+
+        self._loc = new_url
