870dc7441944ad1119b26a54753eb13b913fc8b4
Dmitry Pakhomov <dp@dspro.org>
2019-01-31 15:03:11 +0300

    Add a Markdown extension: SlugsExtension

--- gena/md.py
+++ gena/md.py
@@ -4,12 +4,14 @@ import re
 
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
+from slugify import slugify
 
 from gena.settings import settings
 
 
 __all__ = (
     'SettingsExtension',
+    'SlugsExtension',
 )
 
 
@@ -35,3 +37,25 @@ class SettingsExtension(Extension):
 
     def extendMarkdown(self, md):
         md.preprocessors.register(SettingsPreprocessor(md), 'gena_settings', 10)
+
+
+class SlugsPreprocessor(Preprocessor):
+    def __init__(self, *args, **kwargs):
+        pattern = kwargs.pop('pattern', r'(<!\s*(.+?)\s*!>)')
+        self.pattern = re.compile(pattern)
+        self.default = kwargs.pop('default', '')
+        super().__init__(*args, **kwargs)
+
+    def _replace(self, match):
+        return slugify(match.group(2))
+
+    def run(self, lines):
+        new_lines = []
+        for line in lines:
+            new_lines.append(self.pattern.sub(self._replace, line))
+        return new_lines
+
+
+class SlugsExtension(Extension):
+    def extendMarkdown(self, md):
+        md.preprocessors.register(SlugsPreprocessor(md), 'gena_slugs', 10)
