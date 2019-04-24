8377850cfbe108bf3c03fc33ba5930e06d87ef6d
Dmitry Pakhomov <dp@dspro.org>
2019-04-24 16:44:46 +0300

    Markdown: Add a new extension PostLinkExtension

--- gena/md.py
+++ gena/md.py
@@ -2,15 +2,39 @@
 
 from markdown.extensions import Extension
 from markdown.inlinepatterns import InlineProcessor
+from markdown.util import etree
 from slugify import slugify
 
 from gena.settings import settings
 
 
 __all__ = (
+    'PostLinkInlineProcessor',
     'SettingsExtension',
     'SlugExtension',
 )
+
+
+class PostLinkInlineProcessor(InlineProcessor):
+    """The processor for PostLinkExtension."""
+
+    def handleMatch(self, m, data):
+        a = etree.Element('a')
+        a.text = m.group(2)
+        a.set('href', f'{settings.BLOG_POSTS_URL}/{slugify(m.group(2))}')
+
+        return a, m.start(0), m.end(0)
+
+
+class PostLinkExtension(Extension):
+    """Replace all [[ TITLE ]] with blog post links.
+
+    For example, "[[ Gateway Ridge ]]" could be replaced with
+    "<a href="https://example.com/posts/gateway-ridge">Gateway Ridge</a>".
+    """
+
+    def extendMarkdown(self, md):
+        md.inlinePatterns.register(PostLinkInlineProcessor(r'(\[\[\s*(.+?)\s*\]\])'), 'gena_post_links', 200)
 
 
 class SettingsInlineProcessor(InlineProcessor):
